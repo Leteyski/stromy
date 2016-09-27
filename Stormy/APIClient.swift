83849c9e8bc2590ccff32c42024b53017d2e0965
leteyski <ruslan.leteyski@save.co>
2016-09-27 11:31:44 +0200

    JSONTaskwithRequest implemented

--- Stormy/APIClient.swift
+++ Stormy/APIClient.swift
@@ -8,6 +8,9 @@
 
 import Foundation
 
+public let LETNetworkingErrorDomain = "com.leteyski.Stormy.NetworkingError"
+public let MissingHTTPResponseError: Int = 10
+
 typealias JSON = [String: AnyObject]
 typealias JSONTaskCompletion = (JSON?, HTTPURLResponse?, Error?) -> Void
 typealias JSONTask = URLSessionDataTask
@@ -24,7 +27,46 @@ protocol APIClient {
     init(config: URLSessionConfiguration)
     
     func JSONTaskWithRequest(request: URLRequest, completion: JSONTaskCompletion) -> JSONTask
-   // func fetch<T>(request: URLRequest)
+    func fetch<T>(request: URLRequest, parse: (JSON) -> T?, completion: (APIResult<T>) -> Void)
+    
+}
+
+extension APIClient {
+    
+    func JSONTaskWithRequest(request: URLRequest, completion: @escaping JSONTaskCompletion) -> JSONTask {
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            
+            guard let HTTPResponse = response as? HTTPURLResponse else {
+                let userInfo = [
+                    NSLocalizedDescriptionKey: NSLocalizedString("Missing HTTP Response", comment: "")
+                ]
+                
+                let error = NSError(domain: LETNetworkingErrorDomain, code: MissingHTTPResponseError, userInfo: userInfo)
+                completion(nil, nil, error)
+                
+                return
+            }
+            
+            if data == nil {
+                if let error = error {
+                    completion(nil, HTTPResponse, error)
+                }
+            } else {
+                switch HTTPResponse.statusCode {
+                case 200: do {
+                    let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String : AnyObject]
+                    completion(json, HTTPResponse, nil)
+                } catch let error as NSError {
+                    completion(nil, HTTPResponse, error)
+                    }
+                default: print("Received HTTP Response: \(HTTPResponse.statusCode) - not handled")
+                }
+            }
+            
+        }
+        return task
+    }
     
 }
 
